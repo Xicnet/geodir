4f0a458909088cae059f5046aeb8c6be44407354
Ramiro Augusto Cosentino <rama@xicnet.com>
2019-01-16 01:46:40 +0100

    Implement GraphQL

--- src.bak/app/app.module.ts
+++ src.bak/app/app.module.ts
@@ -6,31 +6,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-import { HttpHandler } from '@angular/common/http'
-import { HttpModule } from '@angular/http'
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClient } from '@angular/common/http';
 import { ApolloModule, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 
 @NgModule({
-	declarations: [AppComponent, ListComponent],
+	declarations: [AppComponent],
 	entryComponents: [],
 	imports: [
 		BrowserModule,
-    HttpClientModule,
 		IonicModule.forRoot(),
 		AppRoutingModule
 	],
 	providers: [
-    HttpLink,
 		StatusBar,
-		Apollo,
 		SplashScreen,
 		{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
 	],
@@ -42,7 +35,7 @@ export class AppModule {
 		httpLink: HttpLink
 	) {
 		apollo.create({
-      link: httpLink.create({uri: 'https://vm8mjvrnv3.lp.gql.zone/graphql'}),
+			link: httpLink.create({ uri: '[URL]' }),
 			cache: new InMemoryCache()
 		});
 	}
