b94e3a11da12dcacddf9a4bb7031d7fce0c372a4
Ramiro Augusto Cosentino <rama@xicnet.com>
2019-01-16 03:54:22 +0100

    Get UFC listings

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import gql from 'graphql-tag';
@@ -9,30 +10,78 @@ import mutation from 'graphql-tag';
 import { Course, Query } from '../types';
 
 const login = gql`
-  mutation submitRepository {
-    createToken(username: "rama", password: "asdasdasd") {
-      token
+	mutation submitRepository {
+		createToken(username: "rama", password: "asdasdasd") {
+			token
+		}
+	}
+`;
+
+const listings = gql`
+query($token: String) {
+  viewer(token: $token) {
+    allUfcs {
+      id
+      title
+      tagline
+      description
+      address
+      hours
+      phone
+      website
+      twitter
+      faircoinAddress
+      lat
+      lng
+      image
     }
   }
+}
 `;
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+	selector: 'app-tabs',
+	templateUrl: 'tabs.page.html',
+	styleUrls: ['tabs.page.scss']
 })
 export class TabsPage implements OnInit {
-  courses: Observable<any>;
-  constructor(private apollo: Apollo) { }
+	token: Observable<any>;
+  listings: any[];
+  loading = true;
+  error: any;
+  private querySubscription: Subscription;
 
-  ngOnInit() {
-    console.log("bla");
-    this.apollo.mutate<any>({
-      mutation: login
-    }).subscribe(({ data }) => {
-      console.log('got data', data);
-    },(error) => {
-      console.log('there was an error sending the query', error);
-    });
+	constructor(private apollo: Apollo) { }
+
+	ngOnInit() {
+    this.getToken();
+    console.log("Token: ", this.token);
   }
+
+	getToken() {
+		console.log("* Getting token");
+		this.apollo.mutate<any>({
+			mutation: login
+		}).subscribe(({ data }) => {
+			console.log('got data', data);
+			let token = data.createToken.token;
+			this.getListing(token);
+		},(error) => {
+			console.log('there was an error sending the query', error);
+		});
+	}
+
+	getListing(token) {
+		console.log("* Getting listing");
+    this.querySubscription = this.apollo
+      .watchQuery({
+        query: listings,
+        variables: {
+          token: token,
+        },
+      })
+      .valueChanges.subscribe(({data}) => {
+        console.log("Listing data: ", data);
+      });
+	}
 }
