d080a62c248b4ef6b96f67efe0ca888e2420e02f
Ramiro Augusto Cosentino <rama@xicnet.com>
2019-01-16 03:31:07 +0100

    Authenticate against graphql

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,18 +7,74 @@ import gql from 'graphql-tag';
 
 import { Course, Query } from '../types';
 
+/*
+
+mutation {
+  createToken(username: "testUser11222", password: "123456") {
+    token
+  }
+}
+const query1($token: String) {
+  viewer(token: $token) {
+    ufc(id:1) {
+      id
+      title
+      tagline
+      description
+      address
+      hours
+      phone
+      website
+      twitter
+      faircoinAddress
+      lat
+      lng
+      image
+    }
+  }
+}
+
+const query($token: String) {
+  viewer(token: $token) {
+    allUfcs {
+      id
+      title
+      tagline
+      description
+      address
+      hours
+      phone
+      website
+      twitter
+      faircoinAddress
+      lat
+      lng
+      image
+    }
+  }
+}
+
+*/
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-  courses: Observable<Course[]>;
+  courses: Observable<any>;
   constructor(private apollo: Apollo) { }
 
   ngOnInit() {
-    this.courses = this.apollo.watchQuery<Query>({
+    this.courses = this.apollo.watchQuery<any>({
       query: gql`
+mutation {
+  createToken(username: "rama", password: "asdasdasd") {
+    token
+  }
+}
+      `
+/*
         query allCourses {
           allCourses {
             id
@@ -29,7 +85,7 @@ export class ListComponent implements OnInit {
             url
           }
         }
-      `
+*/
     })
       .valueChanges
       .pipe(
