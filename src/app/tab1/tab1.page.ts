d766da57e7400af86ee7684c597f9e51b6dd2f83
Rama <code@xicnet.com>
2019-01-22 02:11:02 +0100

    Implement marker clustering

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,164 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
+import { HttpClient } from '@angular/common/http';
+//import { map } from 'rxjs/operators';
+import leaflet from 'leaflet';
+
+declare let L;
+import 'leaflet';
+import 'leaflet.markercluster';
+
+
+function hexToRGBA(hex,opacity){
+  hex = hex.replace('#','');
+  var r = parseInt(hex.substring(0,2), 16);
+  var g = parseInt(hex.substring(2,4), 16);
+  var b = parseInt(hex.substring(4,6), 16);
+
+  var result = 'rgba('+r+','+g+','+b+','+opacity+')';
+  return result;
+}
+
+const apiUrl = "https://use.fair-coin.org/wp-json/custom/v1/all-posts";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  @ViewChild('map') mapContainer: ElementRef;
+  markers: {};
+  map: any;
+  iconUfcSpot: any
+  iconYellow: any
+  iconSelf: any
+  myMarker: any;
+  locations: any;
+  selflayer: any;
+  layers: any;
+
+  constructor(public http: HttpClient
+  ) {
+    this.iconUfcSpot = leaflet.icon({
+      iconUrl: '/assets/imgs/ufcspot.png',
+      shadowUrl: '/assets/imgs/ufcspot-shadow.png',
+      iconSize: [64, 64],
+      iconAnchor: [32, 0],
+      popupAnchor: [-16, 0],
+    });
+
+    this.iconYellow = leaflet.icon({
+      iconUrl: '/assets/imgs/yellow.png',
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+    });
+
+    this.iconSelf = leaflet.icon({
+      iconUrl: '/assets/imgs/me.png',
+      shadowUrl: '/assets/imgs/me-shadow.png',
+      iconSize: [128, 128],
+      iconAnchor: [128, 100],
+    });
+  }
+
+  ionViewDidEnter() {
+    this.loadmap();
+  }
+
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+
+    leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+    }).addTo(this.map);
+    //this.addMyMarker();
+    this.locations = leaflet.markerClusterGroup();
+    this.getListingJSON();
+  }
+  /* Get listings using JSON */
+  getListingJSON() {
+    console.log("* Getting listing");
+
+    this.http.get(apiUrl)
+      .subscribe(data => {
+        console.log(data);
+        this.updateMarkers(data);
+        //layers: leaflet.Layer[ data ];
+      });
+  }
+
+  updateMarkers(items) {
+    var marker = {};
+    var image = "";
+    var description = "";
+    items.response.forEach(item => {
+      var lat = item.geometry.coordinates[1];
+      var lon = item.geometry.coordinates[0];
+      var icon = leaflet.divIcon({
+        className: 'markerInvisible',
+        popupAnchor:  [10, 0], // point from which the popup should open relative to the iconAnchor
+        html: `<img style='width:30px;height:30px;border: 2px solid `+item.properties.icon_marker_color+`;
+        box-shadow: 0 0 5px 4px ` + hexToRGBA(item.properties.icon_marker_color, 0.2) + `
+          ;' class='marker' src='statics/img/categories/` + item.properties.icon_name + `_black.svg' />`
+        // html: "<img style='width:30px;height:30px;border: 2px solid " + hexToRGBA(item.properties.icon_marker_color, 0.2) + ";' class='marker marker-" + item.properties.icon_marker_color + "' src='statics/img/categories/" + item.properties.icon_name + "_black.svg' />"
+      });
+      marker[item.properties.id] = leaflet.marker([lat, lon], {icon: this.iconUfcSpot});
+
+      if(item.properties.image!=null) {
+        console.log( item.properties.image);
+        if(item.properties.image.length) {
+          image = `<img src="`+item.properties.image[0]+`"/>`;
+        }
+      }
+      if(item.properties.description!=null) {
+        description = item.properties.description;
+      }
+
+      marker[item.properties.id].bindPopup("<b>"+item.properties.name+"</b><br/>"+description+image);
+
+      marker[item.properties.id].on('mouseover', function(e) {
+        this.openPopup();
+      });
+      marker[item.properties.id].on('mouseout', function(e) {
+        this.closePopup();
+      });
+      marker[item.properties.id].on('click', function(e) {
+        // show popup?
+        //this.openPopup();
+      });
+      marker[item.properties.id].addTo(this.locations);
+    });
+    this.map.addLayer(this.locations);
+  }
+
+  addMyMarker() {
+    this.map.locate({
+      watch: true,
+      setView: false,
+      maxZoom: 18
+    }).on('locationfound', (e) => {
+      this.selflayer = leaflet.featureGroup();
+      this.map.addLayer(this.selflayer);
+
+      if(this.myMarker) {
+        console.log("* Updating marker");
+        // Update marker position
+        this.myMarker.setLatLng([e.latitude, e.longitude]);
+        this.myMarker.setIcon(this.iconSelf);
+      } else {
+        this.map.setView([e.latitude, e.longitude], 12);
+        // Create marker
+        this.myMarker = leaflet.marker([e.latitude, e.longitude], {icon: this.iconSelf}).on('click', function(e) {
+          this.openPopup();
+        });
+        this.myMarker.setZIndexOffset(999999);
+      }
+      this.selflayer.addLayer(this.myMarker);
+    }).on('locationerror', (err) => {
+      console.log("ERROR: ", err.message);
+    })
+
+  }
+}
