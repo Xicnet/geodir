1ff14999176fde8570153824b6f7dfd567ae1638
Rama <code@xicnet.com>
2019-01-22 11:27:42 +0100

    Cleanup

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NavController, ModalController, PopoverController } from '@ionic/angular';
-import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
 import { Subscription } from 'rxjs';
-
-import gql from 'graphql-tag';
-import mutation from 'graphql-tag';
-
-import { PopoverPage } from '../popover/popover.page';
-
 
 
 @Component({
@@ -18,32 +11,15 @@ import { PopoverPage } from '../popover/popover.page';
 })
 export class TabsPage implements OnInit {
   token: Observable<any>;
-  listings: any[];
   loading = true;
-  error: any;
   private querySubscription: Subscription;
 
-  constructor(private apollo: Apollo, public navCtrl: NavController, private modalCtrl: ModalController,
-    private popoverController: PopoverController,
+  constructor(public navCtrl: NavController
   ) {
 
-  }
-
-  async openPopover(ev: any) {
-    const popover = await this.popoverController.create({
-      component: PopoverPage,
-      event: ev,
-      componentProps: {
-        custom_id: 1
-      }
-    });
-    await popover.present();
   }
 
   ngOnInit() {
   }
 
-
-
-
-}
+} /* end class TabsPage */
