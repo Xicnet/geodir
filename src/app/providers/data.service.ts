5cb58d1d1c6acd70415a82996136f889c1c69ca2
Rama <code@xicnet.com>
2019-01-24 22:39:44 +0100

    Implement filtered listing

--- src/app/providers/data.service.ts
+++ src/app/providers/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+
+const apiUrl = "https://use.fair-coin.org/wp-json/custom/v1/all-posts";
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,8 @@ import { map } from 'rxjs/operators';
 export class DataService {
   items: any;
 
-  constructor() {
-    this.items = [
+  constructor(public http: HttpClient) {
+    var _tems = [
       {title: 'one'},
       {title: 'two'},
       {title: 'three'},
@@ -17,11 +19,26 @@ export class DataService {
       {title: 'five'},
       {title: 'six'}
     ]
+    this.getGeoJSON();
+  }
+
+  getGeoJSON() {
+    this.http.get(apiUrl)
+      .subscribe(data => {
+        this.items = data;
+        //console.log(data);
+      });
   }
 
   filterItems(searchTerm) {
-    return this.items.filter((item) => {
-      return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
+    if(this.items===undefined) {
+      console.log("no items");
+      return;
+    }
+    console.log("items: ", this.items===undefined, typeof this.items, this.items.response);
+    return this.items.response.filter((item) => {
+      console.log(item);
+      return item.properties.name.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     });
   }
 }
