0165a7202bc71653fb786dbd20cc41138b6dc3fc
Rama <code@xicnet.com>
2019-03-16 14:29:11 +0100

    Display listing contact email on info page

--- src/app/shared/details/details.component.ts
+++ src/app/shared/details/details.component.ts
@@ -34,6 +34,9 @@ export class DetailsComponent implements OnInit {
     this.links.openTwitter(url);
   }
 
+  mailto(email) {
+    this.links.mailto('mailto:'+email);
+  }
   geoZoom(location) {
     this.navCtrl.navigate(`/tabs/map?location=${location}`, {});
     this.closeModal();
